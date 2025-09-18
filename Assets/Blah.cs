0ec037aeab551a316bda4213b1abd4e4a4489497
Nitsua15 <cnaustin15@gmail.com>
2025-09-18 12:17:02 -0400

    Update Blah.cs

--- Assets/Blah.cs
+++ Assets/Blah.cs
@@ -5,8 +5,7 @@ public class Blah : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print(("Hello World"));
-        print(("Hello World"));
+    
     }
 
     // Update is called once per frame
